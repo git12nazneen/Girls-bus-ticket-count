4800b61feaf1ca86e2ac8d1fa54c6ff6afb88171
git12nazneen <lipinazneen65@gmail.com>
2024-02-19 14:16:30 -0600

    disable done

--- script/script.js
+++ script/script.js
@@ -1,116 +1,51 @@
-// title count
+
 let totalPrice = 0;
+let selectedSeatNumber = 0;
 
+const keyButtons = document.querySelectorAll('.kbd');
 
+for (const btn of keyButtons) {
+    btn.addEventListener('click', function (e) {
+       
+        const btnDisabled = document.getElementById('current-seats').innerText;
+        const btnParse = parseFloat(btnDisabled)
+        btn.disabled = true;
+        if(btnParse === 36) {
+            btn.disabled = true;
+            return
+        }        
+        setBackGroundColorElement(e.target.id);
+        // seat price
+        const seatPriceElement = document.getElementById('seatPrice');
+        const priceText = seatPriceElement.textContent.trim();
+        const priceValue = parseFloat(priceText);
 
-const keyButton = document.querySelectorAll('.kbd');
-    
-// console.log(keyButton)
-for (const btn of keyButton) {
-   btn.addEventListener('click', function (e){
-     console.log(e.target.innerText)
-     setBackGroundColorElement(e.target.id);
+        // calculate total price
+        totalPrice += priceValue;
+        document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
 
-    //  seat price
-    const seatPriceElement = document.getElementById('seatPrice');
-    // console.log(seatPriceElement);
-    const priceText = seatPriceElement.textContent.trim();
-    const priceValue = parseFloat(priceText);
-    // console.log('ppp', priceValue)
-    // calculate
-    totalPrice += priceValue;
-    document.getElementById('totalPrice').innerText = totalPrice.toFixed(2)
-    
-    
-    //  seat kome 1ta kore
-  
-const currentSeat = document.getElementById('current-seats');
-// console.log('seat', currentSeat)
-    const currentSeatText = currentSeat.innerText;
-    const currentScore = parseInt(currentSeatText)
-    // console.log('seat', currentScore)
-    const newScore = currentScore - 1;
-// currentSeatText.innerText = newScore;
-    // console.log(newScore)
-    currentSeat.innerText = newScore;
+        // decrease available seats
+        const currentSeat = document.getElementById('current-seats');
+        const currentScore = parseInt(currentSeat.innerText);
+        const newScore = currentScore - 1;
+        currentSeat.innerText = newScore;
 
-// available
+        // booking seat
+        const bookingSeatElement = document.getElementById('booked');
+        const currentBookSeat = bookingSeatElement.innerText;
+        const newBooked = parseInt(currentBookSeat) + 1;
+        bookingSeatElement.innerText = newBooked;
 
-    const keyButton = document.querySelectorAll('.kbd')
-    for (let i = 0; i < keyButton.length; i++) {
-       keyButton[i].addEventListener('click', function (){
-         let currentSeat = parseInt(document.getElementById('current-seats').innerText);
-         if(currentSeat > 0){
-            currentSeat--;
-            document.getElementById('current-seats').innerText = currentSeat;
-        }
-       });
-        
-    }
-
-  
-
-
-    // booking seat
-    const bookingSeatElement = document.getElementById('booked');
-    const currentBookSeat = bookingSeatElement.innerText;
-    const currentBook = parseInt(currentBookSeat);
-    // console.log(currentBook, 'book')
-    const newBooked = currentBook + 1;
-    // console.log(newBooked);
-    bookingSeatElement.innerText = newBooked;
-
-
-}
-   
-   )
-
-
-}
-// append child
-const allKeys = document.querySelectorAll("#all-keys .kbd");
-
-allKeys.forEach(key => {
-    key.addEventListener("click", e => {
+        // append child
         const tableBody = document.getElementById('seatBody');
-
         const newRow = document.createElement('tr');
         newRow.innerHTML = `
             <td>${e.target.id}</td>
             <td>Economy</td>
             <td>550</td>
         `;
-        
         tableBody.appendChild(newRow);
+
+       
     });
-});
-
-
-    
-let selectedSeatNumber = 0;
-//    kkkkk
-for (let i = 0; i < keyButton.length; i++) {
-   
-    keyButton[i].addEventListener('click', function (){
-      if(!this.disabled){
-        let currentSeat = parseInt(document.getElementById('current-seats').innerText);
-        if(currentSeat > 0 && selectedSeatNumber < 5){
-            selectedSeatNumber++;
-            this.disabled = true;
-            if(selectedSeatNumber === 4){
-                alert('You have selected 4 seats');
-                for (let j = 0;j < keyButton.length;j++) {
-              
-                    if(!keyButton[j].disabled){
-                        keyButton[j].disabled = true;
-                    }
-                }
-                    
-                }
-            }
-        }
-      }
-    )
 }
-    
-// uuuuuu
