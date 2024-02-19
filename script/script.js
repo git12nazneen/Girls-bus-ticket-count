0197f76f2006fc50d6a4555493e05069c90a0c3d
git12nazneen <lipinazneen65@gmail.com>
2024-02-19 13:41:31 -0600

    append

--- script/script.js
+++ script/script.js
@@ -9,6 +9,7 @@ const keyButton = document.querySelectorAll('.kbd');
 for (const btn of keyButton) {
    btn.addEventListener('click', function (e){
      console.log(e.target.innerText)
+     setBackGroundColorElement(e.target.id);
 
     //  seat kome 1ta kore
   
@@ -30,11 +31,42 @@ const currentSeat = document.getElementById('current-seats');
     const newBooked = currentBook + 1;
     // console.log(newBooked);
     bookingSeatElement.innerText = newBooked;
-   }
+
+    // append child
+//    const tableBody = document.getElementById('seatBody')
+//    console.log(tableBody)
+//    tableBody.innerHTML = '';
+
+//    const row = document.createElement('tr');
+//     console.log(row)
+//     row.innerHTML = `
+//     <td>${e.target.id}</td>
+//     <td>Economy</td>
+//     <td>550</td>
+//     `
+    
+//     tableBody.appendChild(row);
+
+}
    
    )
 
 
 }
 
+const allKeys = document.querySelectorAll("#all-keys .kbd");
 
+allKeys.forEach(key => {
+    key.addEventListener("click", e => {
+        const tableBody = document.getElementById('seatBody');
+
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+            <td>${e.target.id}</td>
+            <td>Economy</td>
+            <td>550</td>
+        `;
+        
+        tableBody.appendChild(newRow);
+    });
+});
